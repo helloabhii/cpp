2c8b9c695cd5629882ff6c1fb83b80d547993d3a
Abhishek Verma <ak8888780@gmail.com>
2024-03-19 22:36:04 +0530

    type conversion

---
+++ chapter-08/typeConversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  // type conversion 
+  double d{3}; // int value 3 implicitly converted to type double 
+  d = 6; // int value 6 implicitly converted to type double 
+ /* 
+  if (5) // int value 5 implicitly converted into type bool
+  {
+  }
+  */
+}
