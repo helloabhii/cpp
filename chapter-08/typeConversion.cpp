d593dcaf01badedde7021e3cf8b1b4b5e0f98bf6
Abhishek Verma <ak8888780@gmail.com>
2024-03-29 00:03:47 +0530

    type conversion

--- chapter-08/typeConversion.cpp
+++ chapter-08/typeConversion.cpp
@@ -9,5 +9,29 @@ int main()
   if (5) // int value 5 implicitly converted into type bool
   {
   }
-  * /
+  */
+  // C-style casts 
+  int x{10};
+  int y{4};
+  double z{(double)x/y}; // convert x to a double so we get floating point division 
+  std::cout << z << '\n'; //prints 2.5     
+
+  // static_cast 
+  char c{'a'};
+  std::cout << c << ' ' << static_cast<int>(c) << '\n'; // prints a 97
+  
+// Type aliases
+using Distance = double; //define Distance as an alias for type double 
+Distance milesToDestination{3.4}; //define a variable of type double                           
+std::cout  << milesToDestination << '\n'; // prints a double value  
+
+// following aliases are identical 
+typedef long Miles;
+using Miles = long;
+
+
+void printDistance(Distance distance); // Distance is some defined type
+
+return 0;
 }
+
